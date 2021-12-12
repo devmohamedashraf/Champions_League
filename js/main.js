84a0398e0f3194cc132231884a8492a3e58ff6b8
unknown <mohamedashraf15353@gmail.com>
2021-12-11 19:41:39 -0800

    Upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function addClass() {
+    var addActive = document.getElementById("sidebar");
+    addActive.classList.add("active");
+ }
+ function removeClass() {
+   var element = document.getElementById("sidebar");
+   element.classList.remove("active");
+ }
